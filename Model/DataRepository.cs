c84c53067175687449c44af24e45e5c040ef1810
matee98 <matzab37@gmail.com>
2020-10-15 19:59:22 +0200

    IFiller interface declaration

---
+++ Model/DataRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model
+{
+    class DataRepository
+    {
+        private readonly DataContext _context;
+
+        public DataRepository(IFiller filler)
+        {
+
+        }
+    }
+}
