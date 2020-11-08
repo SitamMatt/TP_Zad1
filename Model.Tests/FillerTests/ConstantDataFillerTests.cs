548c49ad5fe7b0d037ea5a2c4d63eb102111f017
matee98 <matzab37@gmail.com>
2020-11-08 19:43:12 +0100

    ConstantDataFiller tests

--- Model.Tests/FillerTests/ConstantDataFillerTests.cs
+++ Model.Tests/FillerTests/ConstantDataFillerTests.cs
@@ -1,3 +1,9 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Model.Data;
+using Model.Fillers;
+using Model.Repository;
 using NUnit.Framework;
 
 namespace Model.Tests.FillerTests
@@ -6,7 +12,29 @@ namespace Model.Tests.FillerTests
 	{
 		[Test]
 		public void ConstantDataFiller_CorrectnesTest(){
-			
+			DataContext context = new DataContext();
+			ConstantDataFiller filler = new ConstantDataFiller();
+			filler.Fill(context);
+			DataRepository repository = new DataRepository(filler);
+
+			Assert.AreEqual(repository.GetClient(0).Firstname, "Jan");
+			Assert.AreEqual(repository.GetClient(0).Lastname, "Kowalski");
+			Assert.AreEqual(repository.GetClient(1).Firstname, "Jaros³aw");
+			Assert.AreEqual(repository.GetClient(1).Lastname, "Nowak");
+
+			Assert.AreEqual(repository.GetBook("fff-aahh").Author, "Micha³ Milowicz");
+			Assert.AreEqual(repository.GetBook("hhshasdad").Author, "Jan Marczewski");
+
+			Assert.AreEqual(repository.GetBookCopy(0).PurchaseDate, DateTime.Parse("2004-04-07"));
+			Assert.AreEqual(repository.GetBookCopy(1).PurchaseDate, DateTime.Parse("2005-09-01"));
+			Assert.AreEqual(repository.GetBookCopy(3).PurchaseDate, DateTime.Parse("2006-08-01"));
+
+			Assert.AreEqual(repository.GetBookEvent(0).Client.Lastname, "Kowalski");
+			Assert.AreEqual(repository.GetBookEvent(1).Date, DateTime.Parse("2018-06-01"));
+			Assert.AreEqual(repository.GetBookEvent(2).BookCopy.PurchaseDate, DateTime.Parse("2006-08-01"));
+
+
+
 		}
 	}
 }
