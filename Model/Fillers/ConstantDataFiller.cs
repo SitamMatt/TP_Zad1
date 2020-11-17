4ef4e67b0b26bbb8fd9fd126eb58563cb2a7c986
matee98 <matzab37@gmail.com>
2020-11-17 22:36:03 +0100

    ConstantDataFiller some fixes

--- Model/Fillers/ConstantDataFiller.cs
+++ Model/Fillers/ConstantDataFiller.cs
@@ -10,15 +10,15 @@ namespace Model.Fillers
 {
     public class ConstantDataFiller : IDataFiller
     {
-        private List<Client> clients = new List<Client>();
-        private Dictionary<string, Book> books = new Dictionary<string, Book>();
-        private ObservableCollection<BookCopy> bookCopies = new ObservableCollection<BookCopy>();
-        private ObservableCollection<BookEvent> events = new ObservableCollection<BookEvent>();
+        private List<Client> clients;
+        private Dictionary<string, Book> books;
+        private ObservableCollection<BookCopy> bookCopies;
+        private ObservableCollection<BookEvent> events;
         public ConstantDataFiller(
-            List<Client> clients = null,
-            Dictionary<string, Book> books = null,
-            ObservableCollection<BookCopy> copies = null,
-            ObservableCollection<BookEvent> events = null
+            List<Client> clients,
+            Dictionary<string, Book> books,
+            ObservableCollection<BookCopy> copies,
+            ObservableCollection<BookEvent> events
             )
         {
             this.clients = clients;
